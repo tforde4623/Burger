d1de0c38d75127ae51205cb5ad6ecb12edbc865f
tt4623 <sltebr33zy@gmail.com>
2021-01-05 23:48:15 -0600

    added connection setup

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+// may need to change this when deployed to heroku
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "46234623",
+  database: "employees"
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log("Connected as ID: " + connection.threadId);
+});
+
+
+module.exports = connection;
