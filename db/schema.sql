f0c68e1b39c6d442da0e33f8194c0ee1bcb2f973
tt4623 <sltebr33zy@gmail.com>
2021-01-05 23:47:05 -0600

    schema done and seed file for population done

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE IF NOT EXISTS burgers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES
+("Cheeseburger Deluxe", false),
+("Baconator", false),
+("Chipotle Burger", false);
