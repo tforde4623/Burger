73c2cc51f4f7966e7ee1c57247fa3605f7d09125
tt4623 <sltebr33zy@gmail.com>
2021-01-08 22:46:17 -0600

    added mysql connection info for localhost

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   port: "3306",
   user: "root",
   password: "46234623",
-  database: "employees"
+  database: "burgers_db"
 });
 
 connection.connect(err => {
