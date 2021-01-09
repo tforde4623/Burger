bd22202c91cbf886f555f0ada808b7225fb46291
tt4623 <sltebr33zy@gmail.com>
2021-01-08 22:48:08 -0600

    added dcreate burger listener on submit

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,6 +1,6 @@
 //added in order to attach handlers after rendered (doesn't work without it)
 $(() => {
-  
+
   // listener to change status to devoured
   $('.devour_button').on('click', function(event) {
     const id = $(this).data('id');
@@ -11,5 +11,16 @@ $(() => {
   });
 
   // listener for adding a burger
-  $()
+  $('.create_burger').on('submit', function (event) {
+    event.preventDefault();
+
+    $.ajax({
+      url: '/api/burgers',
+      type: 'POST',
+      data: {
+        burger_name: $('#name_input').val()
+      }
+    }).then(location.reload());
+  });
+
 });
