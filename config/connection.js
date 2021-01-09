edf9704df982364384263bfd5b054d08d3afcb0e
tt4623 <sltebr33zy@gmail.com>
2021-01-08 23:58:22 -0600

    had to fix error in what instructions had in example

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql');
-const connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  const connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
