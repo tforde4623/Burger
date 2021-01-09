6d7f9b44b5bb38782a879b849f23e28e2c8b33b1
tt4623 <sltebr33zy@gmail.com>
2021-01-08 22:59:08 -0600

    server basics set up

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 
 // setting up public as a static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -18,14 +16,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // importing routes and allowing server to use them
-const routes = require('./controllers/catsController');
+const routes = require('./controllers/burgers_controller');
 
-app.use(routes());
+app.use(routes);
 
 // start server and begin listening for requests
 app.listen(PORT, err => {
   if (err) throw err;
-
   console.log(`Server listening on port ${PORT}... \n`);
 });
 
