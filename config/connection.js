bb6052d3382c8da874b9b09649642e87ab5923fa
tt4623 <sltebr33zy@gmail.com>
2021-01-09 21:14:06 -0600

    got rid of commented out old connection config

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,6 @@
 const mysql = require('mysql');
 
 let connection;
-// const source = {
-//   localhost: {
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: "46234623",
-//     database: 'burgers_db'
-//   },
-
-//   jawsDB: {
-//     host: "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//     port: 3306,
-//     user: "ud0ohqg5v3w75ns9",
-//     password: "tn3uvvx43sq0ej9q",
-//     database: "neog1j444f9oxm7v"
-//   }
-// };
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
