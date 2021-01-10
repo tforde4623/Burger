94b435c9aa55f0634cfe0c815f1d80269cc48bc0
tt4623 <sltebr33zy@gmail.com>
2021-01-09 20:27:50 -0600

    changed config for connection

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,34 @@
 const mysql = require('mysql');
 
-const source = {
-  localhost: {
+// const source = {
+//   localhost: {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: "46234623",
+//     database: 'burgers_db'
+//   },
+
+//   jawsDB: {
+//     host: "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "ud0ohqg5v3w75ns9",
+//     password: "tn3uvvx43sq0ej9q",
+//     database: "neog1j444f9oxm7v"
+//   }
+// };
+
+if (process.env.JAWSDB) {
+  const connection = mysql.createConnection(process.env.JAWSDB);
+}
+else {
+  const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: "46234623",
     database: 'burgers_db'
-  },
-
-  jawsDB: {
-    host: "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "ud0ohqg5v3w75ns9",
-    password: "tn3uvvx43sq0ej9q",
-    database: "neog1j444f9oxm7v"
-  }
-};
-
-if (process.env.JAWSDB) {
-  const connection = mysql.createConnection(source.jawsDB);
-}
-else {
-  const connection = mysql.createConnection(source.localhost);
+  });
 }
 
 connection.connect(err => {
